20bafe9fe65d72e5a9e22f0caa49c58654f90383
Victor Magela <victormateusamato@outlook.com>
2026-03-27 22:18:34 -0300

    Fully implements the logic for adding a new card with js

--- script.js
+++ script.js
@@ -27,11 +27,15 @@ function addBookToLibrary(title, author, pages, hasRead) {
     const newBook = new Book(title, author, pages, hasRead);
 
     myLibrary.push(newBook);
+
+    addBookCard(newBook);
 }
 
 const modal = document.getElementById("modal-container");
 const opnBtn = document.getElementById("open");
 const form = document.getElementById("book-form");
+
+const cardContainer = document.querySelector(".card-container");
 
 opnBtn.addEventListener("click", () => {
     modal.classList.add("show");
@@ -60,3 +64,41 @@ form.addEventListener("submit", (e) => {
     
     modal.classList.remove("show");
 });
+
+function addBookCard(book) {
+    const titleLabel = document.createElement("span");
+    titleLabel.textContent = "Title: ";
+    titleLabel.classList.add("label");
+    const authorLabel = document.createElement("span");
+    authorLabel.textContent = "Author: ";
+    authorLabel.classList.add("label");
+    const pagesLabel = document.createElement("span");
+    pagesLabel.textContent = "Number of Pages: ";
+    pagesLabel.classList.add("label");
+    const hasReadLabel = document.createElement("span");
+    hasReadLabel.textContent = "Has Read: ";
+    hasReadLabel.classList.add("label");
+
+    const displayTitle = book.title;
+    const displayAuthor = book.author;
+    const displayPages = book.pages;
+    const displayHasRead = !book.hasRead ? "No" : "Yes";
+
+    const pTitle = document.createElement("p")
+    pTitle.append(titleLabel, displayTitle)
+    const pAuthor = document.createElement("p");
+    pAuthor.append(authorLabel, displayAuthor);
+    const pPages = document.createElement("p");
+    pPages.append(pagesLabel, displayPages);
+    const pHasRead = document.createElement("p");
+    pHasRead.append(hasReadLabel, displayHasRead);
+
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+    bookCard.appendChild(pTitle);
+    bookCard.appendChild(pAuthor);
+    bookCard.appendChild(pPages);
+    bookCard.appendChild(pHasRead);
+
+    cardContainer.appendChild(bookCard);
+}
